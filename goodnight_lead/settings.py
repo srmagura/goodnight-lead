de73903dd07e69c8ae6b58ea800819266369a2e6
Sam Magura <srmagura@gmail.com>
2014-05-23 12:10:43 -0400

    Get it to say "Hello world"

--- goodnight_lead/settings.py
+++ goodnight_lead/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -40,17 +41,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'goodnight_lead.urls'
 
 WSGI_APPLICATION = 'goodnight_lead.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -71,9 +61,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(default='postgres://sam:pw@localhost/goodnight_lead')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
--- goodnight_lead/urls.py
+++ goodnight_lead/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
+from app import views
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'hellodjango.views.home', name='home'),
+    url(r'^$', views.login, name='login'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
