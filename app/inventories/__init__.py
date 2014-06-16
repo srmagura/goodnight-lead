93a6cd5c5d4a2c417cdbaca126e1b78fa1122eb4
Sam Magura <srmagura@gmail.com>
2014-06-16 08:34:29 -0400

    Create Tolerance for Ambiguity inventory

--- app/inventories/__init__.py
+++ app/inventories/__init__.py
@@ -1,9 +1,12 @@
 from shared import InventoryForm
+
 from big_five import BigFive
 from core_self import CoreSelf
 from career_commitment import CareerCommitment
+from ambiguity import Ambiguity
 
-inventoryById = {0: BigFive, 1: CoreSelf, 2: CareerCommitment}
+inventoryById = {0: BigFive, 1: CoreSelf, 2: CareerCommitment,
+    3: Ambiguity}
 
 for inventory_id, inventory in inventoryById.items():
     inventory.inventory_id = inventory_id
