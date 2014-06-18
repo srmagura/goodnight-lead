604b3767d4538930c3a53009a6a92e9d3ad42374
Sam Magura <srmagura@gmail.com>
2014-06-18 14:07:03 -0400

    Finish FiroB except for scoring and results
    Support for multipage inventories is nearly complete.

--- app/inventories/core_self.py
+++ app/inventories/core_self.py
@@ -48,10 +48,7 @@ class CoreSelf(Inventory):
         
         self.metrics = {'score': score}    
             
-    def review_add_data(self, data):
-        if not data['metrics']:
-            return
-            
-        data['score'] = int(data['metrics'][0].value)
+    def review_process_metrics(self, data, metrics):         
+        data['score'] = int(metrics[0].value)
     
         
