82ddb7e94ebe557fe6e6a969ddfacf4c459b6961
EricHorton <ewhorton@ncsu.edu>
2014-06-28 01:12:12 -0400

    Navbar 2.0
    base.html: Implementing a new and improved navbar design which solves
    several issues present in the old one. It also looks cleaner on mobile.
    collapse: renaming as base.js
    base.js: adding jquery needed for the navbar.
    base.css: adding custom css needed for the navbar.

--- app/static/app/js/base.js
+++ app/static/app/js/base.js
@@ -6,9 +6,33 @@ $(window).load(
         //Get the initial window size - breakpoint at 768px (bootstrap xs breakpoint)
         $size = ($(window).width() >= 768) ? "full" : "mobile";
         $prev_size = $size;
-    
+
         //Toggle the initial collapse (for mobile devices or small browser windows)
         toggleCollapse();
+
+        //Set the behavior for .navbar-toggle
+        $('.navbar-toggle').click(function() {
+            //If the nav is collapsed, show the correct menu and
+            //slide the nav out
+            if($('.navbar-slide').width() == 0) {
+                $($(this).data('target')).show();
+                $('.navbar-slide').animate({width: "40%"}, 500);
+            }
+            //If the nav is not collapsed and the toggle for the
+            //currently shown menu is pressed, hide the navbar-slider
+            //and then hide the menus
+            else if($($(this).data('target')).css('display') == "block") {
+                $('.navbar-slide').animate({width: "0%"}, 500, function(){
+                        $('.navbar-collapse').hide();
+                });
+            }
+            //If the nav is not collapsed, and the toggle for the other menu
+            //is pressed, hide the current menu and display the other
+            else {
+                $($(this).data('target')).slideDown(500);
+                $('.navbar-collapse').not($(this).data('target')).slideUp(500);
+            }
+        });
     }
 );
 
@@ -17,12 +41,22 @@ $(window).resize(
     function(){
         //Get the current size
         $size = ($(window).width() >= 768) ? "full" : "mobile";
-        
+
         //If the size has changed
         if($size != $prev_size) {
             toggleCollapse();
+
+            //Make sure the .navbar-slide has the appropriate width
+            //Hide the collapses
+            if($size == "full") {
+                $('.navbar-slide').width("100%");
+                $('.navbar-collapse').hide();
+            }
+            else {
+                $('.navbar-slide').width("0%");
+            }
         }
-        
+
         //Store previous size
         $prev_size = $size;
     }
@@ -38,10 +72,10 @@ toggleCollapse = function() {
     //Collapse the collapse for mobile
     else {
         $(".collapse-xs").removeClass("in");
-        
+
         //Make sure the chevron points the correct direction.
         $span = $(document).find(".visible-xs-icon");
-        
+
         if($span.hasClass("glyphicon-chevron-down")) {
             $span.removeClass("glyphicon-chevron-down");
             $span.addClass("glyphicon-chevron-up");
@@ -52,7 +86,7 @@ toggleCollapse = function() {
 //Rotates the chevron icon in the expand link to indicate expanded/collapsed
 rotateChevron = function($this) {
     $span = $this.find(".visible-xs-icon");
-    
+
     if($span.hasClass("glyphicon-chevron-up")) {
         $span.removeClass("glyphicon-chevron-up");
         $span.addClass("glyphicon-chevron-down");
