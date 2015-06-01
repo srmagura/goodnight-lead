87a465dbb611bbc3336519b58841b8ecf7970630
EricHorton <ewhorton@ncsu.edu>
2015-06-01 18:12:31 -0400

    Editing shared to comply with linter. Commenting.

--- app/inventories/shared.py
+++ app/inventories/shared.py
@@ -1,14 +1,32 @@
+# Import forms for Question
 from django import forms
+
+# Import models for saving inventory results
+# to the database
 import app.models as models
 
+# Inventory represents an inventory taken by a user.
+# Handles submission of an inventory to save as part
+# of the database using the django model framework.
 class Inventory:
+    # Init Inventory. Set default fields used by
+    # sublcasses to remove linter warnings.
+    def __init__(self):
+        # Fields which get set elsewhere
+        self.submission = None
+        self.inventory_id = None
+        self.answers = None
+        self.metrics = None
 
-    # Default value
-    n_pages = 1
+        # Default value
+        self.n_pages = 1
 
+    # Sets the current submission
     def set_submission(self, submission):
         self.submission = submission
 
+    # Submits the inventory and handles saving
+    # to the database.
     def submit(self, user, form):
         if self.submission is None:
             self.submission = models.Submission()
@@ -31,6 +49,8 @@ class Inventory:
 
             self.save_metrics()
 
+    # Handles the saving of each individual answer
+    # to the database.
     def save_answers(self, form):
         self.answers = {}
         for key, value in form.cleaned_data.items():
@@ -41,6 +61,8 @@ class Inventory:
             answer.save()
             self.answers[answer.question_id] = answer.content
 
+    # Computes the metrics for the inventory and then
+    # saves it to the database.
     def save_metrics(self):
         self.compute_metrics()
         for key, value in self.metrics.items():
@@ -50,6 +72,7 @@ class Inventory:
             metric.value = value
             metric.save()
 
+    # Load the answers corresponding to the submission
     def load_answers(self):
         self.answers = {}
         qs = models.Answer.objects.filter(submission=self.submission)
@@ -61,15 +84,26 @@ class Inventory:
     def review_process_metrics(self, data, metrics):
         pass
 
+    # Overridden by subclasses. Used to compute metrics.
+    def compute_metrics(self):
+        pass
 
+# An Inventory question.
 class Question:
-
     def __init__(self, qid, text):
         self.question_id = qid
         self.text = text
 
+# Question sublcass that has a range of numbers
+# as answer choices. Contains its own answer
+# form field.
 class NumberQuestion(Question):
+    # Fields set by subclasses
+    minimum = None
+    maximum = None
+    choice_labels = None
 
+    # Return the form answer field for the question.
     def get_field(self):
         choices = zip(range(self.minimum, self.maximum+1),
             self.choice_labels)
@@ -79,6 +113,9 @@ class NumberQuestion(Question):
             label=label,
             choices=choices)
 
+# Form for an inventory that is built out of each of the
+# fields for each of the questions contained within the
+# inventory.
 class InventoryForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
@@ -86,4 +123,4 @@ class InventoryForm(forms.Form):
         super(InventoryForm, self).__init__(*args, **kwargs)
 
         for question in inventory.questions:
-           self.fields[str(question.question_id)] = question.get_field()
+            self.fields[str(question.question_id)] = question.get_field()
