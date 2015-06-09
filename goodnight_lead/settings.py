29de50600817f2db077d73562749d40b53aa443c
EricHorton <ewhorton@ncsu.edu>
2015-06-08 20:04:06 -0400

    Reverting to old settings.py and wsgi.py

--- goodnight_lead/settings.py
+++ goodnight_lead/settings.py
@@ -1,18 +1,19 @@
-# Import os
-import os
-
-# Import database url
-import dj_database_url
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'q(1n%=3@_7q-1fsqfvgbyjou_wsnm6t_@xahz3=i0fnnl&*hs='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 TEMPLATE_DEBUG = True
+
 ALLOWED_HOSTS = []
 
 
@@ -37,30 +38,32 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-# Root file for all urls
 ROOT_URLCONF = 'goodnight_lead.urls'
 
-# Root configuration for WSGI
 WSGI_APPLICATION = 'goodnight_lead.wsgi.application'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
+
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-#
-# Static asset configuration
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 # Parse database configuration from $DATABASE_URL
+import dj_database_url
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(default='postgres://sam:pw@localhost/goodnight_lead')
 
@@ -69,6 +72,14 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 #Default url for login page (override django default)
 LOGIN_URL = '/login'
--- goodnight_lead/wsgi.py
+++ goodnight_lead/wsgi.py
@@ -1,10 +1,7 @@
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "goodnight_lead.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-# Set default settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "goodnight_lead.settings")
-
-# Set the application
 application = Cling(get_wsgi_application())
