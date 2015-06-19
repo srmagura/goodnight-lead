3286741ec10242b90c31acf960d3b674ca12672d
Sam Magura <srmagura@gmail.com>
2015-06-19 12:45:16 -0400

    Change database user name to lead_dev
    Improvements to README

--- goodnight_lead/settings.py
+++ goodnight_lead/settings.py
@@ -69,7 +69,7 @@ STATIC_URL = '/static/'
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES = {}
-DATABASES['default'] = dj_database_url.config(default='postgres://sam:pw@localhost/goodnight_lead')
+DATABASES['default'] = dj_database_url.config(default='postgres://lead_dev:pw@localhost/goodnight_lead')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
