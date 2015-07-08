7924265d8b1c10af2b7ac3f6c2ed8c98d8057dae
Sam Magura <srmagura@gmail.com>
2015-07-08 02:26:06 -0400

    view_objects.py: write docstrings

--- gl_site/inventories/view_objects.py
+++ gl_site/inventories/view_objects.py
@@ -1,6 +1,44 @@
+"""
+Objects used for displaying inventory results to the user. All of the
+classes in this module map fairly directly to HTML.
+"""
+
 from django.template.loader import render_to_string
 
+class Slider:
+    """
+    A numberline with defined minimum and maximum values. SliderMarkers are
+    used to show a datapoint on the numberline.
+    """
+
+    def __init__(self, min_value, max_value, markers):
+        """
+        min_value -- minimum value for the numberline (float or int)
+        max_value -- minimum value for the numberline (float or int)
+        markers -- iterable containing SliderMarkers to display on the
+        slider
+        """
+        self.min_value = min_value
+        self.max_value = max_value
+        self.markers = markers
+
+    def render(self):
+        """
+        Return a string containing the HTML for this slider.
+        """
+        data = {
+            'min_value': self.min_value,
+            'max_value': self.max_value,
+            'markers': self.markers
+        }
+
+        return render_to_string('slider.html', data)
+
 class SliderMarker:
+    """
+    A marker on a slider. Basically a small colored box with a small piece
+    of text lies at a certain position along the slider.
+    """
 
     def __init__(self, css_id, label, value):
         self.css_id = css_id
@@ -11,23 +49,12 @@ class SliderMarker:
         else:
             self.value = value
 
-class Slider:
-
-    def __init__(self, min_value, max_value, markers):
-        self.min_value = min_value
-        self.max_value = max_value
-        self.markers = markers
-
-    def render(self):
-        data = {
-            'min_value': self.min_value,
-            'max_value': self.max_value,
-            'markers': self.markers
-        }
-
-        return render_to_string('slider.html', data)
 
 class SliderContainer:
+    """
+    A container for the Slider object that places labels at the left and
+    right ends of the slider.
+    """
 
     def __init__(self, labels, slider):
         self.labels = labels
