e13fda17efd7895e6fce566d11520161b4336087
EricHorton <ewhorton@ncsu.edu>
2015-07-25 11:44:25 -0400

    Adding ability to populate session dropdown based on organization
    Custom form widget renders html options with organization attribute.
    Jquery parses and populates the session input based on the organization
    value.

--- gl_site/static/gl_site/js/statistics.js
+++ gl_site/static/gl_site/js/statistics.js
@@ -2,12 +2,39 @@
  * Statistics view. Document on load.
  */
 $(function() {
-    // Make a jQuery ajax GET request to load organization data.
-    // $.get("/statistics/load_organizations", function(data) {
+    // Store jquery elements in more convenient varialbes
+    var $form = $("#statistics_request_form");
+    var $org = $form.children("#id_organization");
+    var $session = $form.children("#id_session");
 
-    // $("#id_organization").add('test', 'test');
-    // for (var field in data) {
-    //     $("#id_organization")[0].options += new Option(1, field);
-    // }
-    // });
+    // Remove all options but the empty value from the session select.
+    var $options = $session.children().detach("[organization!='']");
+
+    // Bind the form submit action to a custom function
+    // for making an ajax requests.
+    $form.submit(function(e) {
+        // Prevent the default post action from occurring
+        e.preventDefault();
+
+        // Generate post data
+        var post = $(this).serialize();
+
+        // Load the post url
+        var url = $(this).attr("action");
+
+        // Make the POST request
+        $.post(url, post, function(data) {
+            $("#content").html(JSON.stringify(data));
+        });
+    });
+
+    // Bind the organization on change to a function to dynamically
+    // set the values of the session input.
+    $org.change(function() {
+        // Detach all but the empty value
+        $session.children().detach("[organization!='']");
+
+        // Append all sessions matching the organization
+        $session.append($options.filter("[organization='" + $org.val() + "']"));
+    });
 });
--- gl_site/statistics/statistics_form.py
+++ gl_site/statistics/statistics_form.py
@@ -2,12 +2,47 @@ from django import forms
 
 from gl_site.models import Organization, Session
 
+from django.utils.encoding import force_text
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
+
+class SessionWidget(forms.widgets.Select):
+    """ Session Widget to allow custom rendering of select options """
+
+    def render_option(self, selected_choices, option_value, option_label):
+        """ Render options with organization attribute for UI utils.
+            Code based on Django source for forms.widget.Select.
+            Overrides forms.widget.Select render_option.
+        """
+
+        if option_value is None:
+            option_value = ''
+        option_value = force_text(option_value)
+        if option_value in selected_choices:
+            selected_html = mark_safe(' selected="selected"')
+            if not self.allow_multiple_selected:
+                # Only allow for a single selection.
+                selected_choices.remove(option_value)
+        else:
+            selected_html = ''
+
+        try:
+            org = Session.objects.get(name=option_label).organization.id
+        except Session.DoesNotExist:
+            org = ''
+
+        return format_html('<option organization="{}" value="{}"{}>{}</option>',
+                           org,
+                           option_value,
+                           selected_html,
+                           force_text(option_label))
+
 class statistics_request_form(forms.Form):
     """ Form used for requesting statistics from the statistics page """
 
-    organization = forms.ModelChoiceField(queryset=None)
+    organization = forms.ModelChoiceField(queryset=None, required=False)
 
-    session = forms.ModelChoiceField(queryset=None, required=False)
+    session = forms.ModelChoiceField(queryset=None, required=False, widget=SessionWidget)
 
     def __init__(self, organizations, sessions, *args, **kwargs):
         super(statistics_request_form, self).__init__(*args, **kwargs)
