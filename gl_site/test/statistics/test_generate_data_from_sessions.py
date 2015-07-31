f0730b90dc0d1cf7a53fbf99c8c08567023a13f4
EricHorton <ewhorton@ncsu.edu>
2015-07-30 20:57:34 -0400

    Finishing data generation tests.

--- gl_site/test/statistics/test_generate_data_from_sessions.py
+++ gl_site/test/statistics/test_generate_data_from_sessions.py
@@ -6,7 +6,10 @@ from django.test import TestCase
 from gl_site.test.factory import Factory
 
 # Models
-from gl_site.models import Submission, Metric
+from gl_site.models import Metric
+
+# Inventories
+from gl_site.inventories import inventory_cls_list
 
 class TestGenerateData(TestCase):
     """ Test case for generating statistics data from sessions """
@@ -39,16 +42,61 @@ class TestGenerateData(TestCase):
             10 submissions for any inventory
         """
 
-        self.assertEqual(9 * 6, Submission.objects.all().count())
+        # Make staff
+        self.user.is_staff = True
+        self.user.save()
+
+        # Request the data and verify it exists
+        data = generate_data_from_sessions(self.sessions, self.user)
+
+        # One entry for each of the 6 inventories
+        self.assertEqual(6, len(data))
 
     def test_not_enough_user_regular(self):
         """ Regular users should not be able to view statistics without data.
             There must be at least 10 submissions to view.
         """
 
-        pass
+        try:
+            # Request the data
+            data = generate_data_from_sessions(self.sessions, self.user)
+            self.fail('LookupError should have been raised. No data.')
+        except LookupError:
+            pass
 
     def test_enough_data(self):
-        """ Metrics are returned correctly if enough data exists """
+        """ Metrics are returned correctly if enough data exists
+            Independent of whether or not user is staff.
+        """
 
-        pass
+        # Generate the 10th set of data
+        Factory.create_set_of_submissions(self.user)
+
+        # Load the data
+        data = generate_data_from_sessions(self.sessions, self.user)
+
+        # All 6 inventories should be listed
+        self.assertEqual(6, len(data))
+
+        inventories_by_name = {i.__name__: i for i in inventory_cls_list}
+
+        # Validate each inventory
+        for entry in data:
+            # Data is present
+            self.assertIn('inventory', entry)
+            self.assertIn('data', entry)
+
+            # Grab all the metrics
+            metrics = Metric.objects.filter(
+                submission__inventory_id=inventories_by_name[entry['inventory']].inventory_id,
+                submission__user__leaduserinfo__session__in=self.sessions
+            ).count()
+
+            # If not via number of data points is equal to the nubmer of metrics
+            if entry['inventory'] != 'Via':
+                self.assertEqual(metrics, len(entry['data']))
+            # Via datapoints is equal to the number of signature
+            # strengths. 3 for this data set because all the data
+            # is always the same.
+            else:
+                self.assertEqual(3, len(entry['data']))
