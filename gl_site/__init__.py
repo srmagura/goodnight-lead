3b1a70ba44eed816c1e9010b50de688786ce7f26
Sam Magura <srmagura@gmail.com>
2017-02-17 14:14:17 -0500

    Write package-level documentation for gl_site directory

---
+++ gl_site/__init__.py
@@ -0,0 +1,18 @@
+"""
+The gl_site package houses the source code for our Django app, which is
+contained within the Django site.
+
+Here's an overview of the subdirectories and what they contain:
+* accounts: views for account management
+* fixtures: the default values for customizable parts of the site
+* forms: Django Form classes
+* inventories: definition and views for psychological questionnaires
+* management: Django management commands
+* migrations: automatically generated SQL for building the schema
+* quotes: inspirational quotes to be displayed on the dashboard
+* static: CSS, JavaScript, and images
+* static_src: CoffeeScript and GIMP sources for files in the static directory
+* statistics: data analytics pages and functionality
+* templates: Django HTML templates
+* test: unit tests for views, forms, and inventories score computation
+"""
