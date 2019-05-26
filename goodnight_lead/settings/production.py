d49509668c4296c9acf637a4a41349055a58fdd4
EricHorton <ewhorton@ncsu.edu>
2019-05-26 17:19:18 -0400

    Add security settings.

--- goodnight_lead/settings/production.py
+++ goodnight_lead/settings/production.py
@@ -30,6 +30,16 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 SECURE_SSL_REDIRECT = True
 
 
+# Security settings
+SECURE_HSTS_SECONDS = 31536000  # 1 year (in seconds)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
+
 # Parse database configuration from $DATABASE_URL
 DATABASES = {'default': dj_database_url.config()}
 
