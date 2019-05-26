8f09d553a7868d7162fa9266f40d3f630d796bbc
EricHorton <ewhorton@ncsu.edu>
2019-05-26 16:09:54 -0400

    Set default settings for wsgi.

--- goodnight_lead/wsgi.py
+++ goodnight_lead/wsgi.py
@@ -1,5 +1,7 @@
+import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goodnight_lead.settings')
 
 application = Cling(get_wsgi_application())
